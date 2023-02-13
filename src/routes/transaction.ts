f53f7af4f8f9933169075a6dd58521dba2650d0a
VictorMello1993 <victorsmello93@gmail.com>
2023-02-12 22:11:14 -0300

    feat: cookies and global hook for logs

--- src/routes/transaction.ts
+++ src/routes/transaction.ts
@@ -2,29 +2,44 @@ import { FastifyInstance } from "fastify";
 import { knex } from "../database";
 import { z } from "zod";
 import { randomUUID } from "node:crypto";
+import { checkSessionIdExists } from "../middlewares/checkSessionIdExists";
 
 export async function transactionRoutes(app: FastifyInstance) {
-  app.get("/", async () => {
-    const transactions = await knex("transactions").select();
+  app.get("/", { preHandler: [checkSessionIdExists] }, async (request, reply) => {
+    const { sessionId } = request.cookies;
+
+    const transactions = await knex("transactions")
+      .where("session_id", sessionId)
+      .select();
+
     return {
       transactions
     };
   });
 
-  app.get("/:id", async (request) => {
+  app.get("/:id", { preHandler: [checkSessionIdExists] }, async (request) => {
     const getTransactionParamsSchema = z.object({
       id: z.string().uuid()
     });
 
+    const { sessionId } = request.cookies;
     const { id } = getTransactionParamsSchema.parse(request.params);
 
-    const transaction = await knex("transactions").where("id", id).first();
+    const transaction = await knex("transactions").where({
+      session_id: sessionId,
+      id
+    }).first();
 
     return { transaction };
   });
 
-  app.get("/summary", async () => {
-    const summary = await knex("transactions").sum("amount", { as: "amount" }).first();
+  app.get("/summary", { preHandler: [checkSessionIdExists] }, async (request) => {
+    const { sessionId } = request.cookies;
+    const summary = await knex("transactions")
+      .where("session_id", sessionId)
+      .sum("amount", { as: "amount" })
+      .first();
+
     return { summary };
   });
 
--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,12 @@ import cookie from "@fastify/cookie";
 const app = fastify();
 
 app.register(cookie);
+
+// Middleware global, executado em todas as rotas da aplicação
+app.addHook("preHandler", async (request, reply) => {
+  console.log(`[${request.method}] ${request.url}`);
+});
+
 app.register(transactionRoutes, {
   prefix: "transactions"
 });
