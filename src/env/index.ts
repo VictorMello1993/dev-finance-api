72e5c12687a3104523aec6694561c518355a9301
VictorMello1993 <victorsmello93@gmail.com>
2023-02-19 10:40:54 -0300

    Preparing to deploy

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,7 +4,8 @@ import { z } from "zod";
 const envSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3335)
+  PORT: z.coerce.number().default(3335),
+  DATABASE_CLIENT: z.enum(["sqlite", "pg"])
 });
 
 const _env = envSchema.safeParse(process.env);
